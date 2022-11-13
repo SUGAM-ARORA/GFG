c44b33a7ed87789b7857f7f79f066bbbb61bf4ac
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2022-11-13 16:48:22 +0530

    Update and rename Reverse a string using Stack to Reverse a string using Stack.cpp

--- Day 4/Reverse a string using Stack.cpp
+++ Day 4/Reverse a string using Stack.cpp
@@ -1,29 +1,13 @@
 //return the address of the string
 char* reverse(char *S, int len)
 {
-
     stack<char> s;
-
-    
-
     for(int i = 0 ; i < len ; i++){
-
         s.push(S[i]);
-
     }
-
-    
-
     for(int i = 0 ; i < len ; i++){
-
         S[i] = s.top();
-
         s.pop();
-
     }
-
-    
-
     return S;
-
 }
