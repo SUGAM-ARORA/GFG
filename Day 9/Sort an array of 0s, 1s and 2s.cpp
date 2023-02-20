2438016db6be902a57e4cf32252f15bc2dcc982f
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-02-20 15:26:00 +0530

    Create Sort an array of 0s, 1s and 2s.cpp

---
+++ Day 9/Sort an array of 0s, 1s and 2s.cpp
@@ -0,0 +1,41 @@
+void sort012(int a[], int n)
+
+    {
+
+        int l=0;
+
+        int m=0;
+
+        int h =(n-1);
+
+        while(m<=h)
+
+        {
+
+            if(a[m]==0){
+
+                swap(a[l],a[m]);
+
+                l++;
+
+                m++;
+
+            }
+
+            else if(a[m]==1){
+
+                m++;
+
+            }
+
+            else{
+
+                swap(a[m],a[h]);
+
+                h--;
+
+            }
+
+        }
+
+    }
